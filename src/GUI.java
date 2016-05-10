a0b2c7c466fcede09f9fbd2384cc1ba3e6e8a680
unknown <nklier238@FHS-LAB127-L30.fhs.win.fuhsd.org>
2016-05-10 11:14:16 -0700

    Still have extermination problem, but generation is propegating

---
+++ src/GUI.java
@@ -0,0 +1,44 @@
+import processing.core.*;
+
+public class GUI extends PApplet {
+	int c;
+	Simulator sim;
+	Display display;
+
+	public void setup() {
+		size(640, 550); // set the size of the screen.
+		Forest f = new Forest(Forest.DEFAULT_FOREST_DENSE);
+		// Create a simulator
+		sim = new Simulator(f);
+		sim.getForest();
+		// Create the display
+		// parameters: (10,10) is upper left of display
+		// (620, 530) is the width and height
+		display = new Display(this, 10, 10, 620, 530);
+
+		// Set different grid values to different colors
+		display.setColor(Tree.ON_FIRE, color(255, 0, 0)); 
+		display.setColor(Tree.ALIVE, color(0, 255, 0));
+		display.setColor(Tree.DEAD, color(255, 255, 255));
+		// You can use classes instead, for example:
+		// display.setColor(Tree.class, color(0, 255, 0));
+		// display.setColor(Ash.class, color(0, 255, 0));
+		
+		display.setNumCols(100);
+		display.setNumRows(100);
+	}
+
+	@Override
+	public void draw() {
+		background(200);
+		sim.doOneStep(); //add the checking for generation in here as well
+		Simulator.slowDown();
+		Simulator.slowDown();
+		display.drawGrid(sim.getForest());
+		//display.drawGrid(sim.getGrid()); // display the game
+	}
+	
+	public void keyReleased(){
+		sim.doOneStep();
+	}
+}
---
+++ src/Simulator.java
@@ -0,0 +1,118 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Simulator {
+	Forest forest;
+	public static ArrayList<Data> dataSet;
+	
+	public Simulator(Forest f){ //good way to test stuff
+		forest = f;
+		f.fill();
+		for(int x = 0; x < 10; x++){
+		f.setRandomFire();
+		}
+	}
+	
+	public void reset(double dense){
+		forest = new Forest(dense);
+		forest.fill();
+		for(int x = 0; x < 10; x++){
+		forest.setRandomFire();
+		}
+	}
+	
+	public void run(){
+		boolean isAblaze = forest.isThereFire();
+		while(isAblaze){
+			forest.timeStep();
+			isAblaze = forest.isThereFire();
+		}
+		
+		Data thisForest = new Data(forest.getDensity(), forest.assess());
+		dataSet.add(thisForest);
+		//have some sort of while loop that allows this to run unitl no more fire
+		//have a status method that returns the number of trees dead and the number on fire, possible in a status object
+	}
+	
+	public void displayInfo(){
+		for(int x = 0; x < dataSet.size(); x++){
+			System.out.println(dataSet.get(x));
+		}
+	}
+	
+	
+	public Simulator(){
+		dataSet = new ArrayList<Data>();
+	}
+	
+	public void doOneStep(){
+		//slowDown();
+		forest.timeStep();
+	}
+
+	public Forest getForest() {
+		return forest;
+	}
+
+	public void setForest(Forest forest) {
+		this.forest = forest;
+	}
+	
+	public static void slowDown(){
+		for(int x = 0; x < 1000000; x ++){
+			System.out.print("");
+		}
+	}
+	
+	public static String makeDensitiesString(){
+		String builder = "";
+		for(int x = 0; x < dataSet.size(); x++){
+			builder += dataSet.get(x).getDensity();
+			builder += " ";
+			builder += "\n";
+		}
+		return builder;
+	}
+	
+	public static String makePercentsString(){
+		String builder = "";
+		for(int x = 0; x < dataSet.size(); x++){
+			builder += dataSet.get(x).getPercentBurned();
+			builder += " ";
+			builder += "\n";
+		}
+		return builder;
+	}
+	public static void writeData(){
+		writeToFile(makeDensitiesString(), "densities");
+		writeToFile(makePercentsString(), "percentages");
+	}
+	
+	public static void writeToFile(String toWrite, String filename) {
+		try {
+			File file = new File("/CompSci/fireData/" + filename + ".txt");
+
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(toWrite);
+			bw.close();
+
+			System.out.println("Done");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean isBurning(){
+		return forest.isThereFire();
+	}
+
+}
